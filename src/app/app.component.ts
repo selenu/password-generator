3aceaf265863d431b7c17b73a823205f936b175f
selenuygun <selenuygun@gmail.com>
2023-12-03 21:42:43 +0300

    password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,68 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'password';
+
+  password = '';
+  length:number = 0;
+  includeLetters:boolean = false;
+  includeNumbers:boolean = false;
+  includeSymbols:boolean = false;
+
+  
+  modifyLength(value:string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)){
+      // In Angular (as well as in JavaScript/TypeScript), debugger; is a statement used to pause code execution at a specific point.
+      // debugger;
+      this.length=parsedValue
+    }
+
+  }
+
+  modifyLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  modifyNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  modifySymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  buttonClick() {
+    const numbers='1234567890';
+    const letters='abcdefghijklmnopqrstuvwxyz';
+    const symbols='!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters)
+    {
+      validChars += letters;
+    }
+    if (this.includeNumbers)
+    {
+      validChars += numbers;
+    }
+    if (this.includeSymbols)
+    {
+      validChars += symbols;
+    }
+    let generatedPassword = "";
+    for (let i=0;i<this.length;i++){
+      
+      const index=Math.floor(Math.random()*validChars.length);
+      generatedPassword += validChars[index]; 
+    }
+
+    this.password = generatedPassword;
+ 
+  
+  }
+
+
+
 }
